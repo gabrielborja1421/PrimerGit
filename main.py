47842789505df81b4f5f68205262dacd243bce80
borja <203412@ids.upchiapas.edu.mx>
2023-01-28 21:35:46 -0600

    mi primer comit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import json
+if __name__=='__main__':
+    url = 'https://pokeapi.co/api/v2/pokemon?limit=8&offset=0'
+    response = requests.get(url)
+
+
+    if response.status_code == 200:
+        response_json= response.json()
+        results= response_json['results']
+        print(results[2])
+
+        response_json= json.loads(response.text)
+        names= response_json['results']
+        names[2]
+        print(names)
+    
+ 
+    
